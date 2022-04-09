20455d727f048919f09c84ff78bd27c493333900
NovaliX-Dev <novalix@protonmail.com>
2022-04-09 22:19:30 +0200

    Refactored License

--- template/src/window.rs
+++ template/src/window.rs
@@ -1,7 +1,7 @@
 /*
 Window - Window function
-
-{% if license_headers and license == "MIT" %}MIT License
+{% if license_headers and license == "MIT" %}
+MIT License
 Copyright (c) {{year}} {{git-username}}
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,7 +21,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-{% endif %}{% if license_headers and license == "Apache v2" %}Copyright {{year}} {{git-username}}
+{% endif %}{% if license_headers and license == "Apache v2" %}
+Copyright {{year}} {{git-username}}
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
